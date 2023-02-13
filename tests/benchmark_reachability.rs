774de04780a5403fd64dd46113e480dcc042afd7
Gijs Leemrijse <32793639+GPLeemrijse@users.noreply.github.com>
2023-02-13 12:08:29 +0100

    More benchmark results

--- tests/benchmark_reachability.rs
+++ tests/benchmark_reachability.rs
@@ -1,5 +1,4 @@
 use std::time::Instant;
-use assert_cmd::Command;
 use std::fs;
 use std::str;
 
@@ -28,7 +27,7 @@ fn test_reachability_benchmarks() {
             let bin_comp_time = compile_cuda_to_bin(&cu_file, &bin_file);
             println!("Completed in {} seconds.", bin_comp_time);
             println!("Running {} on GPU.", file_name);
-            let time = time_binary(&bin_file);
+            let (time, _, _) = time_binary(&bin_file, 100);
             println!("Completed in {} ms.\n", time);
             
             measurements.push((n as u32, m as u32, time, ps as u32, adl_comp_time, bin_comp_time));
@@ -43,20 +42,41 @@ fn test_reachability_benchmarks() {
     fs::write("tests/benchmarks/reachability/results.csv", csv).expect("Unable to write csv file.");
 }
 
-fn time_binary(file : &str) -> f64 {
+#[test]
+fn test_different_optimisations() {
+    let (std_time, std_time_min, std_time_max) = time_binary("./profile_app_no_advises", 100);
+    let (std_both_adv, std_both_adv_min, std_both_adv_max) = time_binary("./profile_app_with_both_advises", 100);
+    let (std_prefetch_no_mng, std_prefetch_no_mng_min, std_prefetch_no_mng_max) = time_binary("./profile_app_no_adv_but_prefetching", 100);
+    let (std_prefetch_all, std_prefetch_all_min, std_prefetch_all_max) = time_binary("./profile_app_prefetch_data_stack_and_managers", 100);
+    let (std_ready_moved_up, std_ready_moved_up_min, std_ready_moved_up_max) = time_binary("./profile_app_moved_ready_up", 100);
+
+    println!("Standard: {} ms {} ms {} ms", std_time, std_time_min, std_time_max);
+    println!("Both adv: {} ms {} ms {} ms", std_both_adv, std_both_adv_min, std_both_adv_max);
+    println!("Prefetch no adv but prefetch: {} ms {} ms {} ms", std_prefetch_no_mng, std_prefetch_no_mng_min, std_prefetch_no_mng_max);
+    println!("Prefetch all: {} ms {} ms {} ms", std_prefetch_all, std_prefetch_all_min, std_prefetch_all_max);
+    println!("Ready moved: {} ms {} ms {} ms", std_ready_moved_up, std_ready_moved_up_min, std_ready_moved_up_max);
+
+}
+
+fn time_binary(file : &str, n : u32) -> (f64, f64, f64) {
     let mut avg = 0.0;
-    let n = 100;
+    let mut min_time : f64 = f64::MAX;
+    let mut max_time : f64 = 0.0;
 
     println!("000/{:03}", n);
     for i in 0..n {
         let out = std::process::Command::new(file)
         .output()
-        .expect("binary would not start.");
+        .expect(&format!("binary {} would not start.", file));
         let time_str = str::from_utf8(&out.stderr).unwrap();
-        avg += time_str[..time_str.len()-3].parse::<f64>().unwrap();
+        let time = time_str[..time_str.len()-4].parse::<f64>().unwrap();
+        avg += time;
+        min_time = f64::min(min_time, time);
+        max_time = f64::max(max_time, time);
+
         println!("{:03}/{:03}", i+1, n);
     }
-    return ((avg / (n as f64))*100.0).round()/100.0;
+    return (((avg / (n as f64))*100.0).round()/100.0, min_time, max_time);
 }
 
 fn compile_adl_to_cuda(file_in: &str, file_out: &str, nrof_structs: u32) -> u64 {
