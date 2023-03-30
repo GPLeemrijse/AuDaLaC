a351df41a3644bf572236df69b7103e460c8c569
GPLeemrijse <32793639+GPLeemrijse@users.noreply.github.com>
2023-03-30 09:56:44 +0200

    Indentation fix

--- compile_resources/ADL.h
+++ compile_resources/ADL.h
@@ -40,29 +40,29 @@ namespace ADL {
 	};
 
 	static Type parse_type_string(std::string s) {
-        if (s == "Int") return Int;
-        if (s == "Nat") return Nat;
-        if (s == "Bool") return Bool;
-        return Ref;
-    }
+      if (s == "Int") return Int;
+      if (s == "Nat") return Nat;
+      if (s == "Bool") return Bool;
+      return Ref;
+   }
 
-    static constexpr size_t size_of_type(Type t) {
-        if (t == Int) return sizeof(IntType);
-        if (t == Nat) return sizeof(NatType);
-        if (t == Bool) return sizeof(BoolType);
-        return sizeof(RefType);
-    }
+   static constexpr size_t size_of_type(Type t) {
+      if (t == Int) return sizeof(IntType);
+      if (t == Nat) return sizeof(NatType);
+      if (t == Bool) return sizeof(BoolType);
+      return sizeof(RefType);
+   }
 
-    static const IntType default_Int = 0;
+   static const IntType default_Int = 0;
 	static const NatType default_Nat = 0;
 	static const BoolType default_Bool = false;
 	static const RefType default_Ref = 0;
 
-    static constexpr const void* default_value(Type t) {
-        if (t == Int) return &default_Int;
-        if (t == Nat) return &default_Nat;
-        if (t == Bool) return &default_Bool;
-        return &default_Ref;
-    }
+   static constexpr const void* default_value(Type t) {
+      if (t == Int) return &default_Int;
+      if (t == Nat) return &default_Nat;
+      if (t == Bool) return &default_Bool;
+      return &default_Ref;
+   }
 }
 #endif
