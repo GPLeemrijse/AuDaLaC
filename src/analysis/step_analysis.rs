a9d3ed6a74dc71795a9925595eb057a156b04e23
GPLeemrijse <32793639+GPLeemrijse@users.noreply.github.com>
2023-07-11 20:30:55 +0200

    Use full dataflow analysis.

--- src/analysis/step_analysis.rs
+++ src/analysis/step_analysis.rs
@@ -148,108 +148,149 @@ fn externally_referenced_parameters<'a>(
     return result;
 }
 
+struct NewLabelInfo<'a> {
+    pub strct: &'a ADLStruct,
+    pub type_info: &'a HashMap<*const Exp, Vec<Type>>,
+    pub nl_params : HashSet<(&'a String, &'a String)>,
+    pub nl_assignments : HashSet<*const Stat>
+}
+
+type VarMap<'a> = HashMap<&'a String, bool>;
 
 pub fn get_new_label_params<'a>(
     strct: &'a ADLStruct,
     type_info: &'a HashMap<*const Exp, Vec<Type>>,
     step: &'a Step
 ) -> (HashSet<(&'a String, &'a String)>, HashSet<*const Stat>) {
-    use Stat::*;
-    use Exp::*;
 
-    let mut new_label_params : HashSet<(&String, &String)> = HashSet::new();
-    let mut new_label_vars : HashSet<&String> = HashSet::new();
-    let mut new_label_param_assignments : HashSet<*const Stat> = HashSet::new();
+    let mut nl_info = NewLabelInfo {
+        strct,
+        type_info,
+        nl_params: HashSet::new(),
+        nl_assignments: HashSet::new()
+    };
 
-    visit_step::<
-        (&mut HashSet<&String>, &mut HashSet<(&String, &String)>, &mut HashSet<*const Stat>),
-        (&ADLStruct, &'a HashMap<*const Exp, Vec<Type>>),
-        ()
-    >(
-        step,
-        &mut (&mut new_label_vars, &mut new_label_params, &mut new_label_param_assignments),
-        |stat, pair, args| {
-            let strct = args.unwrap().0;
-            let info = args.unwrap().1;
-            let vars : &mut HashSet<&String> = &mut pair.0;
-            let pars : &mut HashSet<(&String, &String)> = &mut pair.1;
-            let par_assignments : &mut HashSet<*const Stat> = &mut pair.2;
+    let mut var_env : VarMap = HashMap::new();
+    for s in &step.statements {
+        get_nl_stat(&s, &mut var_env, &mut nl_info);
+    }
 
-            match stat {
-                Declaration(_, lhs, rhs, _) => {
-                    // Remove any information present from an earlier scope
-                    vars.remove(lhs);
-
-                    let rhs_is_new_label = rhs.is_local_var(strct) && vars.contains(&rhs.get_parts()[0]);
-                    let rhs_is_constructed = matches!(**rhs, Constructor(..));
-
-                    if rhs_is_new_label || rhs_is_constructed {
-                        vars.insert(lhs);
-                    }
-                },
-                Assignment(lhs, rhs, _) => {
-                    let rhs_is_new_label = rhs.is_local_var(strct) && vars.contains(&rhs.get_parts()[0]);
-                    let rhs_is_constructed = matches!(**rhs, Constructor(..));
-                    let lhs_is_loc_var = lhs.is_local_var(strct);
-                    let lhs_local_variable = if lhs_is_loc_var {
-                        Some(&lhs.get_parts()[0])
-                    } else {
-                        None
-                    };
-                    let lhs_param = if !lhs_is_loc_var {
-                        let types = info
-                            .get(&(&**lhs as *const Exp))
-                            .expect("Could not find var expression in type info.");
-                        let parts = lhs.get_parts();
-                        let par_name = parts.last().unwrap();
-
-                        let strct_name = if parts.len() == 1 {
-                            &strct.name
-                        } else {
-                            types[types.len() - 2].name().unwrap()
-                        };
-
-                        Some((
-                            strct_name,
-                            par_name,
-                        ))
-                    } else {
-                        None
-                    };
-
-                    // If assigning to a local variable
-                    if let Some(lhs_var) = lhs_local_variable {
-                        if rhs_is_new_label || rhs_is_constructed {
-                            vars.insert(lhs_var);
-                        } else {
-                            /*  When overwritten with something else,
-                                the variable is no longer new.
-                            */
-                            //vars.remove(lhs_var);
-                            // Commented above out as this does not work well with conditionals/scopes
-                        }
-                    }
-                    // If assigning to a parameter
-                    else if let Some(lhs_par) = lhs_param {
-                        if rhs_is_new_label || rhs_is_constructed {
-                            par_assignments.insert(stat as *const Stat);
-                            pars.insert(lhs_par);
-                        }
-                    } else {
-                        unreachable!();
-                    }
-                },
-                _ => {}
-            }
-        },
-        &Some((strct, type_info)),
-        |_, _, _| {},
-        &None
-    );
-
-    return (new_label_params, new_label_param_assignments);
+    return (nl_info.nl_params, nl_info.nl_assignments);
 }
 
+fn get_nl_stat<'a>(stat: &'a Stat, var_env : &mut VarMap<'a>, nl_info : &mut NewLabelInfo<'a>){
+    use Stat::*;
+    match stat {
+        IfThen(..) => get_nl_ifthen(stat, var_env, nl_info),
+        Declaration(..) => get_nl_declaration(stat, var_env, nl_info),
+        Assignment(..) => get_nl_assignment(stat, var_env, nl_info),
+        InlineCpp(..) => {}
+    }
+}
+
+fn get_nl_ifthen<'a>(ifthen: &'a Stat, var_env : &mut VarMap<'a>, nl_info : &mut NewLabelInfo<'a>){
+    if let Stat::IfThen(_, s_true, s_false, _) = ifthen {
+
+        let mut var_env_true = var_env.clone();
+        for s in s_true {
+            get_nl_stat(&s, &mut var_env_true, nl_info);
+        }
+
+        let mut var_env_false = var_env.clone();
+        for s in s_false {
+            get_nl_stat(&s, &mut var_env_false, nl_info);
+        }
+        merge_var_envs(var_env, &var_env_true, &var_env_false);
+
+    } else {
+        unreachable!();
+    }
+}
+
+fn get_nl_declaration<'a>(decl: &'a Stat, var_env : &mut VarMap<'a>, nl_info : &mut NewLabelInfo<'a>){
+    if let Stat::Declaration(_, lhs, rhs, _) = decl {
+        let rhs_is_new_label = rhs.is_local_var(nl_info.strct)
+                                &&
+                               *var_env.get(&rhs.get_parts()[0]).unwrap_or(&false);
+
+        let rhs_is_constructed = matches!(**rhs, Exp::Constructor(..));
+
+        // lhs is a nl_var if rhs is a nl_var or rhs is a constructor
+        var_env.insert(lhs, rhs_is_new_label || rhs_is_constructed);
+    } else {
+        unreachable!();
+    }
+}
+
+fn get_nl_assignment<'a>(assignment: &'a Stat, var_env : &mut VarMap<'a>, nl_info : &mut NewLabelInfo<'a>){
+    if let Stat::Assignment(lhs, rhs, _) = assignment {
+        let rhs_is_new_label = rhs.is_local_var(nl_info.strct)
+                                &&
+                               *var_env.get(&rhs.get_parts()[0]).unwrap_or(&false);
+        let rhs_is_constructed = matches!(**rhs, Exp::Constructor(..));
+        let lhs_is_loc_var = lhs.is_local_var(nl_info.strct);
+        let lhs_local_variable = if lhs_is_loc_var {
+            Some(&lhs.get_parts()[0])
+        } else {
+            None
+        };
+        let lhs_param = if !lhs_is_loc_var {
+            let types = nl_info.type_info
+                .get(&(&**lhs as *const Exp))
+                .expect("Could not find var expression in type info.");
+            let parts = lhs.get_parts();
+            let par_name = parts.last().unwrap();
+
+            let strct_name = if parts.len() == 1 {
+                &nl_info.strct.name
+            } else {
+                types[types.len() - 2].name().unwrap()
+            };
+
+            Some((
+                strct_name,
+                par_name,
+            ))
+        } else {
+            None
+        };
+
+        // If assigning to a local variable
+        if let Some(lhs_var) = lhs_local_variable {
+            // lhs is a nl_var if rhs is a nl_var or rhs is a constructor
+            var_env.insert(lhs_var, rhs_is_new_label || rhs_is_constructed);
+        }
+
+        // If assigning to a parameter
+        else if let Some(lhs_par) = lhs_param {
+            if rhs_is_new_label || rhs_is_constructed {
+                nl_info.nl_assignments.insert(assignment as *const Stat);
+                nl_info.nl_params.insert(lhs_par);
+            }
+        } else {
+            unreachable!();
+        }
+    } else {
+        unreachable!();
+    }
+}
+
+fn merge_var_envs<'a>(var_env : &mut VarMap<'a>, a : &VarMap<'a>, b : &VarMap<'a>){
+    let all_keys : HashSet<&'a String> = a.keys()
+                                           .cloned()
+                                           .collect::<HashSet<&String>>()
+                                           .union(&b.keys().cloned().collect())
+                                           .cloned()
+                                           .collect();
+    for k in all_keys {
+        let nl_in_a = *a.get(k).unwrap_or(&false);
+        let nl_in_b = *a.get(k).unwrap_or(&false);
+        let nl_in_old = *var_env.get(k).unwrap_or(&false);
+
+        // A variable holds a new_label if it does in at least one of the branches
+        var_env.insert(k, nl_in_a || nl_in_b || nl_in_old);
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -480,6 +521,36 @@ mod tests {
                     }
                     n := new;
                 }
+                step3 {
+                    Node new := Node(null, null);
+                    if n != null then {
+                        new := null;
+                    }
+                    if n != null then {
+                        n := new;
+                    }
+                    n := new;
+                }
+                step4 {
+                    Node new := Node(null, null);
+                    if n != null then {
+                        new := null;
+                    }
+                    if n != null then {
+                        new := null;
+                        n := new;
+                        o.x.o := other(null);
+                    }
+                    n := new;
+                }
+                step5 {
+                    Node new := null;
+                    if n != null then {
+                        new := Node(null, null);
+                        n := new;
+                    }
+                    n := new;
+                }
             }
             struct other(x : Node) {}
             step1",
@@ -491,6 +562,9 @@ mod tests {
         let strct = program.struct_by_name(&"Node".to_string()).unwrap();
         let step1 = strct.step_by_name(&"step1".to_string()).unwrap();
         let step2 = strct.step_by_name(&"step2".to_string()).unwrap();
+        let step3 = strct.step_by_name(&"step3".to_string()).unwrap();
+        let step4 = strct.step_by_name(&"step4".to_string()).unwrap();
+        let step5 = strct.step_by_name(&"step5".to_string()).unwrap();
 
         let (np, np_stores) = get_new_label_params(strct, &type_info, step1);
         assert_eq!(np, HashSet::from_iter([(&"Node".to_string(), &"n".to_string())]));
@@ -499,5 +573,17 @@ mod tests {
         let (np, np_stores) = get_new_label_params(strct, &type_info, step2);
         assert_eq!(np, HashSet::from_iter([(&"Node".to_string(), &"n".to_string())]));
         assert_eq!(np_stores.len(), 1);
+
+        let (np, np_stores) = get_new_label_params(strct, &type_info, step3);
+        assert_eq!(np, HashSet::from_iter([(&"Node".to_string(), &"n".to_string())]));
+        assert_eq!(np_stores.len(), 2);
+
+        let (np, np_stores) = get_new_label_params(strct, &type_info, step4);
+        assert_eq!(np, HashSet::from_iter([(&"Node".to_string(), &"n".to_string()), (&"Node".to_string(), &"o".to_string())]));
+        assert_eq!(np_stores.len(), 2);
+
+        let (np, np_stores) = get_new_label_params(strct, &type_info, step5);
+        assert_eq!(np, HashSet::from_iter([(&"Node".to_string(), &"n".to_string())]));
+        assert_eq!(np_stores.len(), 2);
     }
 }
