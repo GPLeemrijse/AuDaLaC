edb1f50e973055983d0fd65d533d3a013b3a2beb
Gijs Leemrijse <32793639+GPLeemrijse@users.noreply.github.com>
2023-07-23 16:47:53 +0200

    Add new problem sizes

--- tests/benchmark_synthesis.rs
+++ tests/benchmark_synthesis.rs
@@ -30,10 +30,13 @@ fn synthesis_files() -> Vec<TestCase<'static>> {
         (
             "Random",
             vec![
-                ("tests/benchmarks/synthesis/testcases/synthesis_10000_13030.init", Vec::new(),   10000 + 13030),
-                ("tests/benchmarks/synthesis/testcases/synthesis_100000_129856.init", Vec::new(),  100000 + 129856),
-                ("tests/benchmarks/synthesis/testcases/synthesis_1000000_1298699.init", Vec::new(), 1000000 + 1298699),
-                ("tests/benchmarks/synthesis/testcases/synthesis_10000000_13001871.init", Vec::new(),10000000 + 13001871),
+                // ("tests/benchmarks/synthesis/testcases/synthesis_10000_13030.init", Vec::new(),   10000 + 13030),
+                ("tests/benchmarks/synthesis/testcases/synthesis_55000_71400.init", Vec::new(),   55000 + 71400),
+                // ("tests/benchmarks/synthesis/testcases/synthesis_100000_129856.init", Vec::new(),  100000 + 129856),
+                ("tests/benchmarks/synthesis/testcases/synthesis_550000_713755.init", Vec::new(),   550000 + 713755),
+                // ("tests/benchmarks/synthesis/testcases/synthesis_1000000_1298699.init", Vec::new(), 1000000 + 1298699),
+                ("tests/benchmarks/synthesis/testcases/synthesis_5500000_7153328.init", Vec::new(),   5500000 + 7153328),
+                // ("tests/benchmarks/synthesis/testcases/synthesis_10000000_13001871.init", Vec::new(),10000000 + 13001871),
             ],
         )
     ]
