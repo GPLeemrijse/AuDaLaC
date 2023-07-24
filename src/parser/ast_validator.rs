a0085d047eeb9745bd99fad42f2e464a290fcee2
GPLeemrijse <32793639+GPLeemrijse@users.noreply.github.com>
2023-07-24 09:56:42 +0200

    Extract validation error out of ast validator

--- src/parser/ast_validator.rs
+++ src/parser/ast_validator.rs
@@ -1,235 +1,7 @@
+use crate::parser::validation_error::*;
 use crate::parser::ast::*;
-use codespan_reporting::diagnostic::Diagnostic;
-use codespan_reporting::diagnostic::Label;
-use core::ops::Range;
 use std::collections::HashMap;
 use std::collections::HashSet;
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct ValidationError {
-    error_type: ValidationErrorType,
-    context: ErrorContext,
-    loc: Loc,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-enum ValidationErrorType {
-    StructDefinedTwice(Loc),                        // Loc of earlier decl
-    StepDefinedTwice(Loc),                          // Loc of earlier decl
-    ParameterDefinedTwice(String, Loc),             // parameter name, Loc of earlier decl
-    VariableAlreadyDeclared(String, Loc),           // var name, Loc of earlier decl
-    UndefinedType(String),                          // attempted type name
-    UndefinedVar(String),                          // attempted var name
-    UndefinedField(String, String),                 // parent name, field name
-    UndefinedStep,                                  // Step and struct name already in error_context
-    InvalidNumberOfArguments(usize, usize),         // Expected number, supplied number
-    TypeMismatch(Type, Type),                       // Expected type, gotten type
-    InvalidTypesForOperator(Type, BinOpcode, Type), // lhs type, operator, rhs type
-    NoNullLiteralForType(Option<Type>),             // Type of attempted null literal
-    ReservedKeyword(String),                        // Name of reserved keyword.
-}
-
-impl ValidationError {
-    pub fn to_diagnostic(&self) -> Diagnostic<()> {
-        let mut labels: Vec<Label<()>> = Vec::new();
-
-        labels.push(self.primary());
-
-        if let Some(secondary) = self.secondary() {
-            labels.push(secondary);
-        }
-
-        return Diagnostic::error()
-            .with_code(self.code())
-            .with_message(self.message())
-            .with_labels(labels);
-    }
-
-    fn code(&self) -> &str {
-        use ValidationErrorType::*;
-        match self.error_type {
-            StructDefinedTwice(..) => "E001",
-            StepDefinedTwice(..) => "E002",
-            ParameterDefinedTwice(..) => "E003",
-            VariableAlreadyDeclared(..) => "E004",
-            UndefinedType(..) => "E005",
-            UndefinedField(..) => "E006",
-            UndefinedStep => "E007",
-            InvalidNumberOfArguments(..) => "E008",
-            TypeMismatch(..) => "E009",
-            InvalidTypesForOperator(..) => "E010",
-            NoNullLiteralForType(..) => "E011",
-            ReservedKeyword(_) => "E012",
-            UndefinedVar(..) => "E013",
-        }
-    }
-
-    fn secondary(&self) -> Option<Label<()>> {
-        use ValidationErrorType::*;
-        match &self.error_type {
-            StructDefinedTwice(l)
-            | StepDefinedTwice(l)
-            | ParameterDefinedTwice(_, l)
-            | VariableAlreadyDeclared(_, l) => {
-                Some(Label::secondary((), l.0..l.1).with_message("Previous declaration here."))
-            }
-            _ => None,
-        }
-    }
-
-    fn primary(&self) -> Label<()> {
-        Label::primary((), self.loc()).with_message(self.label())
-    }
-
-    fn loc(&self) -> Range<usize> {
-        self.loc.0..self.loc.1
-    }
-
-    fn label(&self) -> String {
-        use ValidationErrorType::*;
-        match &self.error_type {
-            StructDefinedTwice(..) => format!(
-                "Struct {} defined twice.",
-                self.context.struct_name.as_ref().unwrap()
-            ),
-            StepDefinedTwice(..) => format!(
-                "Step {} defined twice.",
-                self.context.step_name.as_ref().unwrap()
-            ),
-            ParameterDefinedTwice(p, _) => format!("Parameter {} defined twice.", p),
-            VariableAlreadyDeclared(v, _) => format!("Variable {} defined twice.", v),
-            UndefinedType(t) => format!("Undefined type {}.", t),
-            UndefinedVar(t) => format!("Undefined variable {}.", t),
-            UndefinedField(f, t) => format!("Undefined field {} of {}.", t, f),
-            UndefinedStep => {
-                if let Some(n) = &self.context.struct_name {
-                    format!(
-                        "The struct {} does not have a step {} defined.",
-                        n,
-                        self.context.step_name.as_ref().unwrap()
-                    )
-                } else {
-                    format!(
-                        "The step {} is not defined for any struct.",
-                        self.context.step_name.as_ref().unwrap()
-                    )
-                }
-            }
-            InvalidNumberOfArguments(e, r) => format!(
-                "The struct {} takes {} arguments, while {} were given.",
-                self.context.struct_name.as_ref().unwrap(),
-                e,
-                r
-            ),
-            TypeMismatch(t1, t2) => format!("Expected type {}, but got {}", t1, t2),
-            InvalidTypesForOperator(l, o, r) => format!(
-                "The operator {} can not be applied to a LHS of type {} and a RHS of type {}",
-                o, l, r
-            ),
-            NoNullLiteralForType(None) => {
-                "The null literal is not defined in this context.".to_string()
-            }
-            NoNullLiteralForType(Some(t)) => {
-                format!("The null literal is not defined for type {}.", t)
-            }
-            ReservedKeyword(kw) => format!("The token '{}' is a reserved keyword.", kw),
-        }
-    }
-
-    fn message(&self) -> &str {
-        use ValidationErrorType::*;
-        match self.error_type {
-            StructDefinedTwice(..) => "Struct defined twice.",
-            StepDefinedTwice(..) => "Step defined twice.",
-            ParameterDefinedTwice(..) => "Parameter defined twice.",
-            VariableAlreadyDeclared(..) => "Variable defined twice.",
-            UndefinedType(..) => "Undefined type.",
-            UndefinedVar(..) => "Undefined variable.",
-            UndefinedField(..) => "Undefined field.",
-            UndefinedStep => "Undefined step",
-            InvalidNumberOfArguments(..) => "Invalid number of arguments supplied.",
-            TypeMismatch(..) => "An invalid type has been given.",
-            InvalidTypesForOperator(..) => "Operator can not be applied to given types.",
-            NoNullLiteralForType(..) => "The null literal is not defined in this context.",
-            ReservedKeyword(_) => "Used a reserved keyword.",
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct ErrorContext {
-    struct_name: Option<String>,
-    step_name: Option<String>,
-}
-
-impl ErrorContext {
-    fn from_block_context(context: &BlockEvaluationContext) -> ErrorContext {
-        let mut struct_name: Option<String> = None;
-        let mut step_name: Option<String> = None;
-        if let Some(s) = context.current_struct_name {
-            struct_name = Some(s.clone());
-        }
-        if let Some(s) = context.current_step_name {
-            step_name = Some(s.clone());
-        }
-
-        ErrorContext {
-            struct_name,
-            step_name,
-        }
-    }
-}
-
-fn is_reserved<'ast>(name: &String) -> bool {
-    let reserved_keywords = HashSet::from(["fprintf", "printf"].map(|e| e.to_string()));
-
-    return reserved_keywords.contains(name);
-}
-
-fn check_no_reserved_keywords_structs<'ast>(
-    structs: &'ast Vec<ADLStruct>,
-    context: &mut BlockEvaluationContext<'ast>,
-) {
-    for s in structs {
-        if is_reserved(&s.name) {
-            context.errors.push(ValidationError {
-                error_type: ValidationErrorType::ReservedKeyword(s.name.clone()),
-                context: ErrorContext::from_block_context(context),
-                loc: s.loc,
-            });
-        }
-    }
-}
-
-fn check_no_reserved_keywords_parameters<'ast>(
-    params: &'ast [(String, Type, Loc)],
-    context: &mut BlockEvaluationContext<'ast>,
-) {
-    for (n, _, l) in params {
-        if is_reserved(n) {
-            context.errors.push(ValidationError {
-                error_type: ValidationErrorType::ReservedKeyword(n.clone()),
-                context: ErrorContext::from_block_context(context),
-                loc: *l,
-            });
-        }
-    }
-}
-
-fn check_no_reserved_keywords_steps<'ast>(
-    steps: &'ast Vec<Step>,
-    context: &mut BlockEvaluationContext<'ast>,
-) {
-    for s in steps {
-        if is_reserved(&s.name) {
-            context.errors.push(ValidationError {
-                error_type: ValidationErrorType::ReservedKeyword(s.name.clone()),
-                context: ErrorContext::from_block_context(context),
-                loc: s.loc,
-            });
-        }
-    }
-}
 
 #[derive(Debug)]
 struct BlockEvaluationContext<'ast> {
@@ -260,6 +32,24 @@ impl<'eval, 'ast> BlockEvaluationContext<'ast> {
     fn pop_var_scope(&mut self) {
         if self.vars.pop().is_none() {
             panic!("Popped non-existing variable scope. Context: {:#?}", self);
+        }
+    }
+}
+
+impl ErrorContext {
+    fn from_block_context(context: &BlockEvaluationContext) -> ErrorContext {
+        let mut struct_name: Option<String> = None;
+        let mut step_name: Option<String> = None;
+        if let Some(s) = context.current_struct_name {
+            struct_name = Some(s.clone());
+        }
+        if let Some(s) = context.current_step_name {
+            step_name = Some(s.clone());
+        }
+
+        ErrorContext {
+            struct_name,
+            step_name,
         }
     }
 }
@@ -321,6 +111,78 @@ pub fn validate_ast<'ast>(
 
     (context.errors, context.var_exp_type_info)
 }
+
+fn is_reserved<'ast>(name: &String) -> bool {
+    let reserved_keywords = HashSet::from([
+        "fprintf",
+        "printf",
+        "stable",
+        "ATOMIC",
+        "STORE",
+        "LOAD",
+        "WLOAD",
+        "ACQLOAD",
+        "WSTORE",
+        "RELSTORE",
+        "SetParam",
+        "WSetParam",
+        "RelSetParam",
+        "FP_SET",
+        "FP_READ",
+        "FP_RESET",
+        "executeStep"
+    ].map(|e| e.to_string()));
+
+    return reserved_keywords.contains(name);
+}
+
+fn check_no_reserved_keywords_structs<'ast>(
+    structs: &'ast Vec<ADLStruct>,
+    context: &mut BlockEvaluationContext<'ast>,
+) {
+    for s in structs {
+        if is_reserved(&s.name) {
+            context.errors.push(ValidationError {
+                error_type: ValidationErrorType::ReservedKeyword(s.name.clone()),
+                context: ErrorContext::from_block_context(context),
+                loc: s.loc,
+            });
+        }
+    }
+}
+
+fn check_no_reserved_keywords_parameters<'ast>(
+    params: &'ast [(String, Type, Loc)],
+    context: &mut BlockEvaluationContext<'ast>,
+) {
+    for (n, _, l) in params {
+        if is_reserved(n) {
+            context.errors.push(ValidationError {
+                error_type: ValidationErrorType::ReservedKeyword(n.clone()),
+                context: ErrorContext::from_block_context(context),
+                loc: *l,
+            });
+        }
+    }
+}
+
+fn check_no_reserved_keywords_steps<'ast>(
+    steps: &'ast Vec<Step>,
+    context: &mut BlockEvaluationContext<'ast>,
+) {
+    for s in steps {
+        if is_reserved(&s.name) {
+            context.errors.push(ValidationError {
+                error_type: ValidationErrorType::ReservedKeyword(s.name.clone()),
+                context: ErrorContext::from_block_context(context),
+                loc: s.loc,
+            });
+        }
+    }
+}
+
+
+
 
 fn check_schedule<'ast>(schedule: &'ast Schedule, context: &mut BlockEvaluationContext<'ast>) {
     use crate::parser::ast::Schedule::*;
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,8 +1,11 @@
 pub mod ast;
 mod ast_validator;
+mod validation_error;
+
 
 lalrpop_mod!(pub grammar, "/parser/grammar.rs"); // synthesized by LALRPOP
 
 pub use ast_validator::validate_ast;
 
+#[allow(dead_code)]
 pub use grammar::*;
